41fcecf56b082da94b7d02e433296ab67ecf68d3
wujimu <hasanseirafi69@gmail.com>
2023-06-12 10:56:08 -0700

    remove multer

--- dist/app.js
+++ dist/app.js
@@ -6,42 +6,42 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-const multer_1 = __importDefault(require("multer"));
+// import multer from "multer";
 const port = process.env.PORT || 3001;
 const app = (0, express_1.default)();
-const upload = (0, multer_1.default)();
+// const upload = multer();
 app.use(express_1.default.json()); // Add this line to parse JSON request bodies
 const publicDirectoryPath = path_1.default.join(__dirname, "public");
 // Default route for all other paths
 app.get("/", (req, res) => {
     res.sendFile(path_1.default.join(publicDirectoryPath, "index.html"));
 });
-app.post("/public", upload.single("file"), (req, res) => {
-    const fileName = req.body.fileName; // Get the file name from the request body
-    const fileData = req.body.file; // Get the file data from the request body
-    const filePath = path_1.default.join(__dirname, "public", fileName);
-    const directoryPath = path_1.default.dirname(filePath);
-    fs_1.default.mkdirSync(directoryPath, { recursive: true });
-    const writeStream = fs_1.default.createWriteStream(filePath);
-    writeStream.on("finish", () => {
-        console.log(`File "${fileName}" written successfully.`);
-        res.status(200).send(`File "${fileName}" written successfully.`);
-    });
-    writeStream.on("error", (err) => {
-        console.error("Error writing file:", err);
-        res.status(500).send("An error occurred while writing the file.");
-    });
-    // Write the file data to the writable stream
-    if (fileData) {
-        writeStream.write(fileData, "utf8");
-    }
-    // Pipe the file buffer into the writable stream
-    if (req.file && req.file.buffer) {
-        writeStream.write(req.file.buffer);
-    }
-    // End the writable stream
-    writeStream.end();
-});
+// app.post("/public", upload.single("file"), (req, res) => {
+//   const fileName = req.body.fileName; // Get the file name from the request body
+//   const fileData = req.body.file; // Get the file data from the request body
+//   const filePath = path.join(__dirname, "public", fileName);
+//   const directoryPath = path.dirname(filePath);
+//   fs.mkdirSync(directoryPath, { recursive: true });
+//   const writeStream = fs.createWriteStream(filePath);
+//   writeStream.on("finish", () => {
+//     console.log(`File "${fileName}" written successfully.`);
+//     res.status(200).send(`File "${fileName}" written successfully.`);
+//   });
+//   writeStream.on("error", (err) => {
+//     console.error("Error writing file:", err);
+//     res.status(500).send("An error occurred while writing the file.");
+//   });
+//   // Write the file data to the writable stream
+//   if (fileData) {
+//     writeStream.write(fileData, "utf8");
+//   }
+//   // Pipe the file buffer into the writable stream
+//   if (req.file && req.file.buffer) {
+//     writeStream.write(req.file.buffer);
+//   }
+//   // End the writable stream
+//   writeStream.end();
+// });
 app.get("/directory-contents/:dirname", (req, res) => {
     const { dirname } = req.params;
     const result = [];
